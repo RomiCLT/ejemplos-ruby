9df33e16e7e538c5cbf561280fca4293f1c180a6
RomiCLT <cuidemoslatierrachile@gmail.com>
2022-05-26 12:51:17 -0400

    ejemplos

---
+++ variables.rb
@@ -0,0 +1,14 @@
+#sumando dos variables
+x = 10
+y = 20
+suma = x + y
+puts suma
+#multiplicando dos variables
+precio_dolar= 20
+peso_mx= 770
+conversion = precio_dolar * peso_mx
+puts conversion
+#variables tipo texto
+texto_saludo= "hola desde Temuco"
+puts texto_saludo
+puts "hasta el próximo código"
