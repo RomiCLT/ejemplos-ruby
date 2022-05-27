ef2231412243b4e4f56854f61fa1441f11c090fa
RomiCLT <cuidemoslatierrachile@gmail.com>
2022-05-27 11:13:46 -0400

    respuestas mensajes

---
+++ strings.rb
@@ -0,0 +1,8 @@
+mensaje = "ya casi son vacaciones"
+respuesta = 'pero tengo desafios pendientes'
+puts mensaje
+puts respuesta
+puts mensaje + respuesta
+puts mensaje * 5
+puts 1.to_s * 3
+puts '7' * 3
