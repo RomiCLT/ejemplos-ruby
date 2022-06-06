70ab06e052dc4ab8c4d4658f2a16258c10fa0252
RomiCLT <cuidemoslatierrachile@gmail.com>
2022-06-06 13:48:32 -0400

    ejercicio edad

---
+++ usando_and_or.rb
@@ -0,0 +1,24 @@
+print "dame tu edad"
+edad= gets.chomp.to_i
+print "¿eres zurdo o diestro?"
+mano_dominante= gets.chomp
+
+# if edad > 18
+#     puts "eres mayor de edad"
+#     if mano_dominante == "zurdo"
+#     puts "eres mayor de edad"
+#     end
+# else
+#     puts "eres menor de edad"
+# end
+if edad > 18 && mano_dominante == "zurdo"
+    puts "eres mayor de edad y zurdo"
+elsif edad > 18 && mano_dominante == "diestro"
+    puts "eres mayor de edad y diestro"
+elsif edad < 18 && mano_dominante == "zurdo"
+    puts "eres menor de edad y zurdo"
+elsif edad < 18 && mano_dominante == "diestro"
+    puts "eres menor de edad y diestro"
+else 
+    puts "opcion capturada no valida"
+end
