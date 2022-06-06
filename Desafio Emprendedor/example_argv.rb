8362232dc28d3d29a6235041dcc936c002f1005e
RomiCLT <cuidemoslatierrachile@gmail.com>
2022-06-06 12:34:59 -0400

    ejemplo argv

---
+++ Desafio Emprendedor/example_argv.rb
@@ -0,0 +1,7 @@
+
+primero = ARGV[0].to_i
+segundo = ARGV[1].to_i
+tercero = ARGV[2].to_i
+suma =primero + segundo + tercero
+puts "la variable de argv  #{primero} + #{segundo} + #{tercero} y la suma #{primero +segundo +tercero}"
+print ARGV.class
