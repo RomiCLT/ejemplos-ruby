4516b72c7f1b19a1ec8dfce966b91d7119a9384a
RomiCLT <cuidemoslatierrachile@gmail.com>
2022-06-08 09:54:17 -0400

    ejemplo listas li

---
+++ generador_li.rb
@@ -0,0 +1,14 @@
+# pedimos valor
+    
+    print "¿cuantos item tendra tu lista html?"
+    n_item= gets.chomp.to_i
+    j=1
+# imprimimos lista
+while j <= n_item 
+puts "<li>#{j}</li>"
+    j+=1 #j=j+1
+
+end
+
+
+
